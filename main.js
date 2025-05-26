963ceac0f78641dda5fc90e9ff790ea398b99444
Geovanna de oliveira Costa 22a <geovannaoliveiracosta10@gmail.com>
2025-05-26 14:28:11 -0300

    Adição da arrow function showItemsList e finalizaçãi em casa

--- main.js
+++ main.js
@@ -14,5 +14,25 @@ function addItem(){
 
     document.querySelector("#item").value = ""
 
-    console.log(items)
+   showItemsList()
+}
+
+function showItemsList(){
+    const sectionList = document.querySelector(".list")
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-1">
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-1">Pão de forma</label>
+                </div>
+                <button>
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>`
+    })
 }
